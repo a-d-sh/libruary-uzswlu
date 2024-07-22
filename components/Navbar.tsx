d93315d62a85ea4e4abe6e8b71c72e10e173f8e9
adsh <ad.sh.1997@mail.ru>
2024-07-22 12:37:50 +0500

    navbar logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,33 @@
-"use client";
-import { UserButton } from "@clerk/nextjs";
-import Link from "next/link";
-import { MdQuiz } from "react-icons/md";
-import UserMenu from "./UserMenu";
+'use client'
+import { UserButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import { MdQuiz } from 'react-icons/md'
+import UserMenu from './UserMenu'
 
 const Navbar = () => {
-  return (
-    <div className="pt-5 w-full">
-      <div className="max-w-[1500px] mx-auto w-[90%] flex justify-between items-center border-b pb-5">
-        <div>
-          <Link
-            href={"/"}
-            className="flex gap-1 items-center text-2xl"
-          >
-            <h1 className="text-dark font-bold">
-              CodeQuiz
-            </h1>
-            <MdQuiz className="text-primary" />
-          </Link>
-        </div>
+	return (
+		<div className='pt-5 w-full'>
+			<div className='max-w-[1500px] mx-auto w-[90%] flex justify-between items-center border-b pb-5'>
+				<div>
+					<Link href={'/'} className='flex gap-1 items-center text-2xl'>
+						<h1 className='text-dark font-bold'>QuizIUSI</h1>
+						<MdQuiz className='text-primary' />
+					</Link>
+				</div>
 
-        <div className="md:block hidden text-nowrap">
-          <span className="bg-primary px-5 py-1 rounded-md text-white">
-            Today's Category: Javascript
-          </span>
-        </div>
+				<div className='md:block hidden text-nowrap'>
+					<span className='bg-primary px-5 py-1 rounded-md text-white'>
+						Today's: Javascript
+					</span>
+				</div>
 
-        <div className="flex items-center gap-3 justify-end">
-          <UserMenu />
-          <UserButton />
-        </div>
-      </div>
-    </div>
-  );
-};
+				<div className='flex items-center gap-3 justify-end'>
+					<UserMenu />
+					<UserButton />
+				</div>
+			</div>
+		</div>
+	)
+}
 
-export default Navbar;
+export default Navbar
