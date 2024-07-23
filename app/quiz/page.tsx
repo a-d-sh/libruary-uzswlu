fe6220198a373472ba1d73a18e3fafc5d877b605
adsh <ad.sh.1997@mail.ru>
2024-07-23 18:27:36 +0500

    test order

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,30 +1,30 @@
-import Quiz from "@/components/Quiz";
-import { client } from "@/sanity/lib/client";
-import { fetchUsers } from "../(auth)/actions/fetchUsers";
+import Quiz from '@/components/Quiz'
+import { client } from '@/sanity/lib/client'
+import { fetchUsers } from '../(auth)/actions/fetchUsers'
 
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic'
 
 async function getData() {
-  const query = `*[_type == "questions"]{
+	const query = `*[_type == "questions"] | order(_createdAt asc) {
     question,
     answers,
     correctAnswer
-  }`;
+  }`
 
-  const data = await client.fetch(query);
+	const data = await client.fetch(query)
 
-  return data;
+	return data
 }
 
 const page = async () => {
-  const questions = await getData();
-  const user = await fetchUsers();
-  const userId = user?.data.user.id;
-  return (
-    <>
-      <Quiz questions={questions} userId={userId} />
-    </>
-  );
-};
+	const questions = await getData()
+	const user = await fetchUsers()
+	const userId = user?.data.user.id
+	return (
+		<>
+			<Quiz questions={questions} userId={userId} />
+		</>
+	)
+}
 
-export default page;
+export default page
