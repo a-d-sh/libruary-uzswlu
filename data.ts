39f31135e06363955ebde8ccc88ecba7080b2f8a
adsh <ad.sh.1997@mail.ru>
2024-07-23 08:39:43 +0500

    test english

--- data.ts
+++ data.ts
@@ -1,125 +1,132 @@
 export const quiz = {
-  totalQuestions: 20,
-  questions: [
-    {
-      id: 1,
-      question: 'What is the result of `typeof typeof 1`?',
-      answers: ['number', 'string', 'object', 'undefined'],
-      correctAnswer: 'string',
-    },
-    {
-      id: 2,
-      question: 'What will `console.log(1 === true)` output?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'false',
-    },
-    {
-      id: 3,
-      question: 'What is the result of `"hello" instanceof Object`?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'false',
-    },
-    {
-      id: 4,
-      question: 'What does `console.log([10] === [10])` return?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'false',
-    },
-    {
-      id: 5,
-      question: 'What will the following code output: `console.log(1 + "2" + "2")`?',
-      answers: ['122', '5', '14', 'undefined'],
-      correctAnswer: '122',
-    },
-    {
-      id: 6,
-      question: 'What is the output of `console.log(3 + 2 + "7")`?',
-      answers: ['12', '327', '57', 'undefined'],
-      correctAnswer: '57',
-    },
-    {
-      id: 7,
-      question: 'What will be the output of `console.log(Math.max())`?',
-      answers: ['0', 'undefined', 'Infinity', 'TypeError'],
-      correctAnswer: '-Infinity',
-    },
-    {
-      id: 8,
-      question: 'What will be the output of `console.log(1 < 2 < 3)`?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'true',
-    },
-    {
-      id: 9,
-      question: 'What is the result of `"5" - 3`?',
-      answers: ['2', '8', 'NaN', 'undefined'],
-      correctAnswer: '2',
-    },
-    {
-      id: 10,
-      question: 'What will `console.log(1 - "1")` output?',
-      answers: ['0', '1', 'undefined', 'TypeError'],
-      correctAnswer: '0',
-    },
-    {
-      id: 11,
-      question: 'What is the output of `console.log(typeof NaN)`?',
-      answers: ['number', 'string', 'NaN', 'undefined'],
-      correctAnswer: 'number',
-    },
-    {
-      id: 12,
-      question: 'What does the following code return: `console.log(typeof (typeof 1))`?',
-      answers: ['number', 'string', 'object', 'undefined'],
-      correctAnswer: 'string',
-    },
-    {
-      id: 13,
-      question: 'What will the following code output: `console.log([] + {})`?',
-      answers: ['[object Object]', '{}', 'undefined', 'TypeError'],
-      correctAnswer: '[object Object]',
-    },
-    {
-      id: 14,
-      question: 'What will the output of `console.log(1 + 2 + "3")` be?',
-      answers: ['33', '123', '6', 'undefined'],
-      correctAnswer: '33',
-    },
-    {
-      id: 15,
-      question: 'What will be the output of `console.log(+"hello")`?',
-      answers: ['NaN', 'SyntaxError', 'undefined', 'TypeError'],
-      correctAnswer: 'NaN',
-    },
-    {
-      id: 16,
-      question: 'What will the following code output: `console.log(!!"false")`?',
-      answers: ['false', 'true', 'undefined', 'TypeError'],
-      correctAnswer: 'true',
-    },
-    {
-      id: 17,
-      question: 'What is the result of `console.log([] + [])`?',
-      answers: ['[]', '{}', '0', 'undefined'],
-      correctAnswer: '',
-    },
-    {
-      id: 18,
-      question: 'What will be the output of `console.log(0.1 + 0.2 === 0.3)`?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'false',
-    },
-    {
-      id: 19,
-      question: 'What will be the output of the following code: `console.log(typeof typeof 1)`?',
-      answers: ['number', 'string', 'object', 'undefined'],
-      correctAnswer: 'string',
-    },
-    {
-      id: 20,
-      question: 'What is the output of `console.log(1 === true)`?',
-      answers: ['true', 'false', 'undefined', 'TypeError'],
-      correctAnswer: 'false',
-    },
-  ],
-};
+	totalQuestions: 20,
+	questions: [
+		{
+			id: 1,
+			question:
+				'A person who manages customers` money effectively and profitably.',
+			answers: ['A seller', 'A banker', 'An architect'],
+			correctAnswer: 'A banker',
+		},
+		{
+			id: 2,
+			question:
+				'A person who plans, designs, and oversees the construction of buildings',
+			answers: ['A seller', 'A banker', 'An architect'],
+			correctAnswer: 'An architect',
+		},
+		{
+			id: 3,
+			question: 'To look after people when they are sick.',
+			answers: ['A nurse', 'A banker', 'A teacher'],
+			correctAnswer: 'A nurse',
+		},
+		{
+			id: 4,
+			question: 'A person who develops websites for clients.',
+			answers: ['A nurse', 'An IT programmer', 'A teacher'],
+			correctAnswer: 'An IT programmer',
+		},
+		{
+			id: 5,
+			question: 'A university academic of the highest rank.',
+			answers: ['A nurse', 'An IT programmer', 'A professor'],
+			correctAnswer: 'A professor',
+		},
+		{
+			id: 6,
+			question:
+				'A person whose job is to keep, inspect, and analyze financial accounts.',
+			answers: ['A nurse', 'An IT programmer', 'An accountant'],
+			correctAnswer: 'An accountant',
+		},
+		{
+			id: 7,
+			question:
+				'A person whose job involves selling and renting out buildings and land for clients.',
+			answers: ['An estate agent', 'An IT programmer', 'An accountant'],
+			correctAnswer: 'An estate agent',
+		},
+		{
+			id: 8,
+			question:
+				'A person whose job involves helping people to sort out their legal affairs.',
+			answers: ['An estate agent', 'A lawyer', 'An accountant'],
+			correctAnswer: 'A lawyer',
+		},
+		{
+			id: 9,
+			question: 'I investigate and write articles to newspapers',
+			answers: ['An estate agent', 'A lawyer', 'A journalist'],
+			correctAnswer: 'A journalist',
+		},
+		{
+			id: 10,
+			question:
+				'A medical practitioner specializing in children and their diseases.',
+			answers: ['An estate agent', 'A pediatrician', 'A journalist'],
+			correctAnswer: 'A pediatrician',
+		},
+		{
+			id: 11,
+			question: 'Plastic makes rivers and oceans dirty.',
+			answers: ['True', 'False'],
+			correctAnswer: 'True',
+		},
+		{
+			id: 12,
+			question: 'It doesn’t kill animals.',
+			answers: ['True', 'False'],
+			correctAnswer: 'False',
+		},
+		{
+			id: 13,
+			question: 'It takes one year to disappear.',
+			answers: ['True', 'False'],
+			correctAnswer: 'False',
+		},
+		{
+			id: 14,
+			question: 'A lot of companies want to replace plastic.',
+			answers: ['True', 'False'],
+			correctAnswer: 'True',
+		},
+		{
+			id: 15,
+			question: 'McDonald’s is trying to make a paper bottle.',
+			answers: ['True', 'False'],
+			correctAnswer: 'False',
+		},
+		{
+			id: 16,
+			question: 'Coca-Cola was world’s biggest plastic polluter in 2019.',
+			answers: ['True', 'False'],
+			correctAnswer: 'True',
+		},
+		{
+			id: 17,
+			question: 'Their aim is to create a paper bottle.',
+			answers: ['True', 'False'],
+			correctAnswer: 'True',
+		},
+		{
+			id: 18,
+			question: 'A paper bottle doesn’t open up a whole new world of packaging',
+			answers: ['True', 'False'],
+			correctAnswer: 'False',
+		},
+		{
+			id: 19,
+			question:
+				'Coca –Cola wants to use only packaging from renewable sources.',
+			answers: ['True', 'False'],
+			correctAnswer: 'True',
+		},
+		{
+			id: 20,
+			question: 'They wouldn’t be good for our planet.',
+			answers: ['True', 'False'],
+			correctAnswer: 'False',
+		},
+	],
+}
