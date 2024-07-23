c0d840464e7bc36a7c044eb4d60e899f0a494780
adsh <ad.sh.1997@mail.ru>
2024-07-23 19:52:13 +0500

    Testdan 1 marta foydalanish

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,7 +1,7 @@
+// quiz/page.tsx
 import Quiz from '@/components/Quiz'
 import { client } from '@/sanity/lib/client'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import { fetchUsers } from '../(auth)/actions/fetchUsers'
 
 export const dynamic = 'force-dynamic'
@@ -24,11 +24,12 @@ const Page = async () => {
 	const user = await fetchUsers()
 	const userId = user?.data.user.id
 
-	useEffect(() => {
-		if (user?.data?.quizResults?.length > 0) {
-			router.push('/stats')
-		}
-	}, [user, router])
+	// Check if the user has taken the quiz
+	if (user?.data?.quizResults?.length > 0) {
+		// Redirect to stats page
+		router.push('/stats')
+		return null // Return null to prevent rendering the quiz page
+	}
 
 	return (
 		<>
