8e4a9de75021ba83cd2278a99b40ecbcbf6b76b6
adsh <ad.sh.1997@mail.ru>
2024-07-23 17:35:41 +0500

    clerk bez email

--- app/(auth)/actions/fetchUsers.ts
+++ app/(auth)/actions/fetchUsers.ts
@@ -1,46 +1,50 @@
-"use server"
-import {prisma} from "@/lib/prisma"
-import { currentUser } from "@clerk/nextjs"
+'use server'
+import { prisma } from '@/lib/prisma'
+import { currentUser } from '@clerk/nextjs'
 
 export const fetchUsers = async () => {
-  try {
-    const clerkUser = await currentUser()
-    let mongoUser = null
-    mongoUser = await prisma.user.findUnique({
-      where: {
-        clerkUserId: clerkUser?.id
-      }
-    })
+	try {
+		const clerkUser = await currentUser()
+		let mongoUser = null
+		mongoUser = await prisma.user.findUnique({
+			where: {
+				clerkUserId: clerkUser?.id,
+			},
+		})
 
-    if (!mongoUser) {
-      let username = clerkUser?.username
-      if (!username) {
-        username = clerkUser?.firstName + " " + clerkUser?.lastName
-      }
-      const newUser: any = {
-        clerkUserId: clerkUser?.id,
-        username,
-        email: clerkUser?.emailAddresses[0].emailAddress,
-        profilePic: clerkUser?.imageUrl
-      }
-      mongoUser = await prisma.user.create({
-        data: newUser
-      })
-    }
+		if (!mongoUser) {
+			let username = clerkUser?.username
+			if (!username) {
+				username = clerkUser?.firstName + ' ' + clerkUser?.lastName
+			}
+			const email =
+				clerkUser?.emailAddresses.length > 0
+					? clerkUser.emailAddresses[0].emailAddress
+					: `${clerkUser?.id}@noemail.com`
+			const newUser: any = {
+				clerkUserId: clerkUser?.id,
+				username,
+				email,
+				profilePic: clerkUser?.imageUrl,
+			}
+			mongoUser = await prisma.user.create({
+				data: newUser,
+			})
+		}
 
-    const quizResults = await prisma.quizResult.findMany({
-      where: {
-        userId: mongoUser.id
-      }
-    })
+		const quizResults = await prisma.quizResult.findMany({
+			where: {
+				userId: mongoUser.id,
+			},
+		})
 
-    return {
-      data: {
-        user: mongoUser,
-        quizResults
-      }
-    }
-  } catch (error) {
-    console.log(error)
-  }
+		return {
+			data: {
+				user: mongoUser,
+				quizResults,
+			},
+		}
+	} catch (error) {
+		console.log(error)
+	}
 }
